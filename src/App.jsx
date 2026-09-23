7ce4a8a3a968766494a11b8e4c5ee6a30a1e5c58
Clarence Matthew Tayo <d.tayoclarencematthew@gmail.com>
2026-09-23 23:33:09 +0800

    1.5: course information step 5

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,45 @@ const Content = ({ course }) => {
 
 const Total = ({ course }) => {
   let total = 0
+
   for (let i = 0; i < course.parts.length; i++) {
     total += course.parts[i].exercises
   }
+
   return <p><strong>Total of {total} exercises</strong></p>
+}
+
+const Footer = ({ name, courseCode, section }) => {
+  return (
+    <footer>
+      <p>{name}</p>
+      <p>{courseCode} - {section}</p>
+    </footer>
+  )
 }
 
 const App = () => {
   const course = {
     name: 'CSIT327 - Information Management 2',
     parts: [
-  { name: 'CSIT321 - Applications Development and Emerging Technologies', exercises: 3 },
-  { name: 'IT317 - Project Management for IT', exercises: 3 },
-  { name: 'RIZAL031 - The Life and Works of Rizal', exercises: 3 },
-  { name: 'CSIT340 - Web Development', exercises: 3 }
+      { name: 'CSIT321 - Applications Development and Emerging Technologies', exercises: 3 },
+      { name: 'IT317 - Project Management for IT', exercises: 3 },
+      { name: 'RIZAL031 - The Life and Works of Rizal', exercises: 3 },
+      { name: 'CSIT340 - Web Development', exercises: 3 }
     ]
   }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
+  <div className="app">
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+    <Footer
+      name="Clarence Matthew D. Tayo"
+      courseCode="CSIT340"
+      section="G6"
+    />
+  </div>
   )
 }
 
